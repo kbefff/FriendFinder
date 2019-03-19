0d1b76c7c797e4a152aa8f0854ab2535ed55b78d
kbefff <kbeffel10@me.com>
2019-03-19 15:56:19 -0700

    friend data

---
+++ app/data/friends.js
@@ -0,0 +1,71 @@
+var friends = [{
+        name: "Archie",
+        photo: "/../assets/images/archie.jpg",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+
+        ]
+    },
+    {
+        name: "Betty",
+        photo: "/../assets/images/archie.jpg",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+
+        ]
+    },
+    {
+        name: "Veronica",
+        photo: "/../assets/images/archie.jpg",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+
+        ]
+    },
+    {
+        name: "Reggie",
+        photo: "/../assets/images/archie.jpg",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+
+        ]
+    }
+];
+
+module.exports = friends;
