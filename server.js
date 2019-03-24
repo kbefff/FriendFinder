cb32612ab587314485578f50bc9abeab941d482c
kbefff <kbeffel10@me.com>
2019-03-24 11:43:31 -0700

    body parser

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
 var app = express();
-
-
 var PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use(express.json());
-
+app.use(bodyParser.json())
 
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
