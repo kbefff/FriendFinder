a511cf18db0afd2c09740e740b5c2710b3640073
kbefff <kbeffel10@me.com>
2019-03-26 12:56:30 -0700

    photo fix

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var friendArray = [{
         name: "Archie",
-        photo: "../assets/images/archie.jpg",
+        photo: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTU3NzgxMzUyMDMxMjAwNDky/_kj-apa-attends-foxs-teen-choice-awards-at-the-forum-on-august-12-2018-in-inglewood-california-photo-by-jon-kopaloff_filmmagic-square.jpg",
         scores: [
             "5",
             "1",
@@ -17,7 +17,7 @@ var friendArray = [{
     },
     {
         name: "Betty",
-        photo: "../assets/images/archie.jpg",
+        photo: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTU2NTg4ODY1OTA0MTkwNzY0/lili-reinhart-arrives-for-the-2018-paleyfest-los-angeles---cws-riverdale-at-dolby-theatre-on-march-25-2018-in-hollywood-california-photo-by-gabriel-olsen_filmmagic-square.jpg",
         scores: [
             "1",
             "1",
@@ -34,7 +34,7 @@ var friendArray = [{
     },
     {
         name: "Veronica",
-        photo: "../assets/images/archie.jpg",
+        photo: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTU3OTEyNTE1NTAwNTgyMjE3/camila-mendes-arrives-at-the-foxs-teen-choice-awards-2018-at-the-forum-on-august-12-2018-in-inglewood-california.jpg",
         scores: [
             "5",
             "1",
@@ -50,8 +50,8 @@ var friendArray = [{
         ]
     },
     {
-        name: "Reggie",
-        photo: "../assets/images/archie.jpg",
+        name: "Jughead",
+        photo: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTU2NDUxMzYzODY4Mzg2ODky/cole-sprouse-attends-the-2018-cw-network-upfront-at-the-london-hotel-on-may-17-2018-in-new-york-city-photo-by-dia-dipasupil_getty-images-square.jpg",
         scores: [
             "3",
             "1",
